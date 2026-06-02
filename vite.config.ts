984c283016285bcd0ca60bc0964f27c69be8893b
Titano12312 <simonebellini2012@gmail.com>
2026-06-02 20:30:20 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -12,4 +12,8 @@ export default defineConfig({
     // nitro/vite builds from this
     server: { entry: "server" },
   },
+  // Sovrascriviamo il target di default di Lovable per forzare il build su Vercel
+  nitro: {
+    preset: "vercel"
+  }
 });
