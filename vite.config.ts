01e11e10dfe6338f7e29708338dd0fb3400c4f81
Titano12312 <simonebellini2012@gmail.com>
2026-06-02 21:41:19 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from "@lovable.dev/vite-tanstack-config";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { tanstackRouterVite } from "@tanstack/router-plugin";
+import { lovableViteConfig } from "@lovable.dev/vite-tanstack-config";
+import tsconfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig({
-  tanstackStart: {
-    server: { entry: "server" },
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => ({
+  plugins: [
+    tanstackRouterVite(),
+    react(),
+    tsconfigPaths(),
+    mode === "development" 
+      ? lovableViteConfig() 
+      : lovableViteConfig({ nitro: true }),
+  ],
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
   },
-});
+}));
