f290938a61e1a5cfbdeb675adfd1486cb13b0142
Titano12312 <simonebellini2012@gmail.com>
2026-06-02 23:25:52 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { tanstackRouterVite } from "@tanstack/router-plugin";
+import { TanStackRouterVite } from "@tanstack/router-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    tanstackRouterVite(),
+    TanStackRouterVite(),
     react(),
     tsconfigPaths(),
   ],
